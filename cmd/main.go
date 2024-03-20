228eb7f8223c57b8164ea49df5f157bd4b6df956
osnatkulyok1 <osnatbird@gmail.com>
2024-03-20 14:46:18 +0200

    initial commit

--- cmd/main.go
+++ cmd/main.go
@@ -15,70 +15,77 @@ import (
 	"github.com/open-policy-agent/opa/topdown"
 )
 
+// Declare a constant for the resource type being evaluated
 const (
 	resourceType = "toy_resource"
 )
 
+// The main function where execution begins
 func main() {
-	ctx := context.Background()
-	// load risk policies
+	ctx := context.Background() // Initialize a new context
+	// Declare variables for error handling and loading policies
 	var err error
 	var policies *loader.Result
 
+	// Construct the absolute path to the policy file
 	policyAbsolutePath, _ := filepath.Abs(fmt.Sprintf("policies/%v/policy.rego", resourceType))
+	// Load the policy file, excluding directories and non-Rego files
 	if policies, err = loader.NewFileLoader().Filtered([]string{policyAbsolutePath}, func(_ string, info os.FileInfo, _ int) bool {
 		return !info.IsDir() && !strings.HasSuffix(info.Name(), bundle.RegoExt)
 	}); err != nil {
-		panic(err)
+		panic(err) // Handle loading errors
 	}
 
+	// Initialize the Rego compiler
 	compiler :=
 		ast.NewCompiler().
-			WithEnablePrintStatements(true).
-			WithStrict(true).
+			WithEnablePrintStatements(true). // Enable print statements in Rego policies
+			WithStrict(true). // Enable strict mode
 			WithUnsafeBuiltins(map[string]struct{}{
-				ast.HTTPSend.Name:   {},
+				ast.HTTPSend.Name:   {}, // Disable unsafe built-in functions
 				ast.OPARuntime.Name: {},
 			})
 
-	// compile risk policies
+	// Compile the loaded policy modules
 	compiler.Compile(policies.ParsedModules())
 	if compiler.Failed() {
-		panic(compiler.Errors)
+		panic(compiler.Errors) // Handle compilation errors
 	}
 
-	// read resource declaration file
+	// Read the resource declaration file
 	resourceDeclarationFileAbsolutePath, _ := filepath.Abs(fmt.Sprintf("policies/%v/resource.json", resourceType))
 	resourceFileContent, err := os.ReadFile(resourceDeclarationFileAbsolutePath)
 	if err != nil {
-		panic(err)
+		panic(err) // Handle file reading errors
 	}
 
 	var resourceFileInput map[string]any
+	// Unmarshal the JSON content of the resource file into a map
 	err = json.Unmarshal(resourceFileContent, &resourceFileInput)
 	if err != nil {
-		panic(err)
+		panic(err) // Handle JSON unmarshalling errors
 	}
 
-	// query the resource declaration file for risks
+	// Prepare a Rego query to evaluate the resource file against the policies
 	var preparedEvalQuery rego.PreparedEvalQuery
 	if preparedEvalQuery, err =
 		rego.New(
-			rego.Compiler(compiler),
-			rego.PrintHook(topdown.NewPrintHook(os.Stdout)),
-			rego.Query("risk_path = data.example.analyze"),
-			rego.Input(resourceFileInput),
+			rego.Compiler(compiler), // Use the compiled policies
+			rego.PrintHook(topdown.NewPrintHook(os.Stdout)), // Set up a print hook for debug printing
+			rego.Query("risk_path = data.example.analyze"), // Define the query to find risk paths
+			rego.Input(resourceFileInput), // Set the input for the query as the resource file content
 		).PrepareForEval(ctx); err != nil {
-		panic(err)
+		panic(err) // Handle query preparation errors
 	}
 
-	// print the resultant risks
+	// Execute the query and print the results
 	var resultSet rego.ResultSet
 	if resultSet, err = preparedEvalQuery.Eval(ctx); err != nil {
-		panic(err)
+		panic(err) // Handle query evaluation errors
 	}
 
+	// Output the identified risks
 	fmt.Println("Risk found in resource type: ", resourceFileInput["type"])
 	fmt.Println("Risk Paths: ", resultSet[0].Bindings["risk_path"])
-	fmt.Println("Risk Lines: <TODO>")
+	fmt.Println("Risk Lines: <TODO>") // Placeholder for future implementation
 }
